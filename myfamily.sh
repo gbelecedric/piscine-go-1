d1e536a2381ca3cf25f2a998c5f1b6a6550d5b2e
Nelson <nelgoug@gmail.com>
2019-06-07 08:36:15 +0200

    myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,3 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r ".[] | select( .id == $HERO_ID ) | .connections.relatives" | cut -d '"' -f2
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq -r ".[] | select( .id == $HERO_ID ) | .connections.relatives" | perl -p -0n -e's/\n/\\n/'
+
 
