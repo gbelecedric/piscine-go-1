303eb01c34871b795a5e289400532eb8cc6e43c0
Nelson <nelgoug@gmail.com>
2019-06-09 01:00:10 +0200

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	PrintComb2()
+}
+
+func PrintComb2() {
+	for a:=0;a<=97;a++{
+		for b:=0;b<=99;b++{
+			if a<b{
+				fmt.Printf("%02d %02d, ",a,b)
+			}
+		}
+	}
+	fmt.Printf("98 99")
+				
+}
