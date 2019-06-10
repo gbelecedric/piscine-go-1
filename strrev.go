6cf8fb2549ff583226fadb0aba659275f9e5c028
Nelson <nelgoug@gmail.com>
2019-06-10 20:41:11 +0200

    strrev

---
+++ strrev.go
@@ -0,0 +1,19 @@
+package piscine
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func StrRev(s string) string {
+	reversed := make([]byte, len(s))
+	i := 0
+
+	for len(s) > 0 {
+		r, size := utf8.DecodeLastRuneInString(s)
+		s = s[:len(s)-size]
+		i += utf8.EncodeRune(reversed[i:], r)
+	}
+
+	return string(reversed)
+}
