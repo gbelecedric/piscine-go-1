71d11fc7ed3dc3832742e546f63b09f2f864a3c9
Nelson <nelgoug@gmail.com>
2019-06-12 03:16:08 +0200

    findnextprime

---
+++ findnextprime.go
@@ -0,0 +1,28 @@
+package piscine
+
+func FindNextPrime(nb int)int{
+	for i>=nb+1{
+		if premier(i){
+			return i
+		}
+		i++	
+	}
+
+}
+
+
+func premier(nb int) bool{
+	decision:=true
+	if nb<=1{
+		return false
+	}
+	for i:=2;i<nb;i++{
+		if nb%i==0{
+			decision=false
+		}
+	}
+	return decision
+}
+
+
+
