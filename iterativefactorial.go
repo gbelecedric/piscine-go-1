ad788c97ca209cefc67bfa12dc35e09c911b79a2
Nelson <nelgoug@gmail.com>
2019-06-12 02:08:55 +0200

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,5 +1,7 @@
 package piscine
 
+import "math"
+
 func IterativeFactorial(n int) int{
 	if n == 0 || n==1{
 		return 1
@@ -7,7 +9,10 @@ func IterativeFactorial(n int) int{
 	if n > 1{
 		fact:=1
 		for i:=1;i<=n;i++{
-			fact*=i	
+			fact*=i
+			if fact > math.MaxInt32{
+				return 0			
+			}
 		}
 		return fact
 	}
