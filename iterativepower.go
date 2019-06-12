e32ae77bdee6a465b77015b53640ccafc1e9103c
Nelson <nelgoug@gmail.com>
2019-06-12 02:23:38 +0200

    iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -3,12 +3,12 @@ package piscine
 import "math"
 
 func IterativePower(nb,power int) int{
-	if n < 0{
+	if power < 0{
 		return 0
 	}
-	if n >=0{
+	if power >=0{
 		result:=1
-		for i:=1;i<=nb;i++{
+		for i:=1;i<=power;i++{
 			result*=nb
 			if result > math.MaxInt32{
 				return 0			
