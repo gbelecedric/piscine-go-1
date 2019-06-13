eb9e44d61e0d804b4ba1b60ca5512bac6e1767dc
Nelson <nelgoug@gmail.com>
2019-06-14 00:46:47 +0200

    nrune

--- nrune.go
+++ nrune.go
@@ -2,7 +2,7 @@ package piscine
 
 import"unicode/utf8"
 
-func NRune(s string, n int) rune{
-	ss,_:=utf8.DecodeRune([]byte(s[n:]))
+func FirstRune(s string, n int) rune{
+	ss,_:=utf8.DecodeRune([]byte(s[n+1:]))
 	return ss
 }
