3e10d03de773f6d1cea943779dce1ba0b985b7de
Nelson <nelgoug@gmail.com>
2019-06-14 19:54:28 +0200

    cpitalize

--- capitalize.go
+++ capitalize.go
@@ -3,5 +3,20 @@ package piscine
 import "strings"
 
 func Capitalize(s string) string{
-	return strings.Title(s)
+	cbs:=[]rune(s)
+	for i,_:=range cbs{
+		if(i==0){
+			if(cbs[i]>=97 &&cbs[i] <=122){//on met la premier lettre en majuscule
+				cbs[i]-=32
+			}
+			
+		}else{
+			if(cbs[i]>=65 &&cbs[i] <=90){//on met les autres lettres en miniscule
+				cbs[i]+=32
+			}
+		}
+		
+	}
+
+	return string(cbs)
 }
