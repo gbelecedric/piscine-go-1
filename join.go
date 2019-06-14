01886ea08ed3021fc80c4d56626a42ac8cc6e95c
Nelson <nelgoug@gmail.com>
2019-06-15 00:04:46 +0200

    join

--- join.go
+++ join.go
@@ -4,16 +4,16 @@ func Join(strs []string, sep string) string{
 	result:=""
 	for i,value:=range strs{
 		if i!=len(strs)-1{
-			result=concat(result,value)
-			result=concat(result,sep)
+			result=concat1(result,value)
+			result=concat1(result,sep)
 		}else{
-			result=concat(result,value)
+			result=concat1(result,value)
 		}
 	}
 	return result	
 }
 
-func concat(str1,str2 string) string{
+func concat1(str1,str2 string) string{
 	result:=make([]byte,len(str1)+len(str2))
 	j:=0
 	for i,_:=range result{
