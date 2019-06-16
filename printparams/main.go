2f392ccdfea85088ae18d384f43978393dc0797e
Nelson <nelgoug@gmail.com>
2019-06-16 03:14:54 +0200

    printparams

--- printparams/main.go
+++ printparams/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-	//"strings"
 	)
 
 func main(){
 	args:=os.Args
-	//cp:=strings.Split(args[1]," ")
 	if len(args)> 1{
 		for i:=1;i<len(args);i++{
 			fmt.Printf("%v\n",args[i])
